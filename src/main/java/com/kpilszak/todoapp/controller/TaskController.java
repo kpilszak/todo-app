40518b25e237bb5bfd131f6bc67cd2b105271964
kpilszak <klaudia.pilszak@gmail.com>
2020-12-30 12:38:16 +0100

    Create TaskController and hide delete crud methods in TaskRepository

---
+++ src/main/java/com/kpilszak/todoapp/controller/TaskController.java
@@ -0,0 +1,34 @@
+package com.kpilszak.todoapp.controller;
+
+import com.kpilszak.todoapp.model.Task;
+import com.kpilszak.todoapp.model.TaskRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.rest.webmvc.RepositoryRestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@RepositoryRestController
+class TaskController {
+    public static final Logger logger = LoggerFactory.getLogger(TaskController.class);
+    private final TaskRepository repository;
+
+    public TaskController(final TaskRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping(value = "/tasks", params = {"!sort", "!page", "!size"})
+    ResponseEntity<List<Task>> readAllTasks() {
+        logger.warn("Exposing all the tasks!");
+        return ResponseEntity.ok(repository.findAll());
+    }
+
+    @GetMapping("/tasks")
+    ResponseEntity<List<Task>> readAllTasks(Pageable page) {
+        logger.info("Custom pageable");
+        return ResponseEntity.ok(repository.findAll(page).getContent());
+    }
+}
