a5a61d15783defc97d17986581e5f5a67a92f154
kpilszak <klaudia.pilszak@gmail.com>
2020-12-30 12:47:12 +0100

    Clean after removing RestRepository

--- src/main/java/com/kpilszak/todoapp/model/TaskRepository.java
+++ src/main/java/com/kpilszak/todoapp/model/TaskRepository.java
@@ -2,13 +2,11 @@ package com.kpilszak.todoapp.model;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
-import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Integer> {
-    @RestResource(path = "done", rel = "done")
     List<Task> findByDone(@Param("state") boolean done);
 }
