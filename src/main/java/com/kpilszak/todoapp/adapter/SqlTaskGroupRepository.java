1527e32c84dd6c0e3e9ef66c9c9fa9631db40ea5
kpilszak <klaudia.pilszak@gmail.com>
2020-12-31 14:23:03 +0100

    Ignore LazyLoading in OneToMany to avoid n+1 problem

--- src/main/java/com/kpilszak/todoapp/adapter/SqlTaskGroupRepository.java
+++ src/main/java/com/kpilszak/todoapp/adapter/SqlTaskGroupRepository.java
@@ -3,8 +3,14 @@ package com.kpilszak.todoapp.adapter;
 import com.kpilszak.todoapp.model.TaskGroup;
 import com.kpilszak.todoapp.model.TaskGroupRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 interface SqlTaskGroupRepository extends TaskGroupRepository, JpaRepository<TaskGroup, Integer> {
+    @Override
+    @Query("from TaskGroup g join fetch g.tasks")
+    List<TaskGroup> findAll();
 }
