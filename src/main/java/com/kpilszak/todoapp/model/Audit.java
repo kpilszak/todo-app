54bf889478aafcfd1aeb362ed82d26a8d726e7de
kpilszak <klaudia.pilszak@gmail.com>
2020-12-31 13:17:02 +0100

    Composition over inheritance with BaseAuditableEntity

--- src/main/java/com/kpilszak/todoapp/model/Audit.java
+++ src/main/java/com/kpilszak/todoapp/model/Audit.java
@@ -1,12 +1,12 @@
 package com.kpilszak.todoapp.model;
 
-import javax.persistence.MappedSuperclass;
+import javax.persistence.Embeddable;
 import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
 import java.time.LocalDateTime;
 
-@MappedSuperclass
-abstract class BaseAuditableEntity {
+@Embeddable
+class Audit {
     private LocalDateTime createdOn;
     private LocalDateTime updatedOn;
 
