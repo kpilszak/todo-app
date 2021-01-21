4b5f5a67b7180d810e37117e905fb14eec416dcc
kpilszak <klaudia.pilszak@gmail.com>
2021-01-21 18:18:45 +0100

    feat: Add entities

--- src/main/java/com/kpilszak/todoapp/model/TaskGroup.java
+++ src/main/java/com/kpilszak/todoapp/model/TaskGroup.java
@@ -15,6 +15,9 @@ public class TaskGroup {
     private boolean done;
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "group")
     private Set<Task> tasks;
+    @ManyToOne
+    @JoinColumn(name = "project_id")
+    private Project project;
 
     public TaskGroup() {
 
@@ -50,5 +53,13 @@ public class TaskGroup {
 
     public void setTasks(Set<Task> tasks) {
         this.tasks = tasks;
+    }
+    
+    Project getProject() {
+        return project;
+    }
+    
+    void setProject(final Project project) {
+        this.project = project;
     }
 }
